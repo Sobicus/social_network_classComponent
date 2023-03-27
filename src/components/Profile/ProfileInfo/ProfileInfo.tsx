0c570303f5c03adefb49591eeef06b66bc6600e0
Sobicus <83043123+Sobicus@users.noreply.github.com>
2023-03-28 01:19:38 +0200

    update socialNetwork v0.3

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -28,7 +28,7 @@ export const ProfileInfo: React.FC<profileInfoType> = (props) => {
     }
     return (
         <div>
-            <div>
+            <div className={style.coverImg}>
                 <img
                     src="https://steamuserimages-a.akamaihd.net/ugc/817811439325303605/ACB13C573377D3D2E4F32FE6ED61B3D082AA5ADC/?imw=1024&imh=576&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
                     alt=""/>
